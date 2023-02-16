c60ebf70107dff0a9cc1e7afb6a3e1de96f8c74a
Carlos Agengo <coagengo@gmail.com>
2023-02-16 03:04:10 -0800

    edited description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main: prints (using printf) - with proper grammar, but the outcome is a piece of art,
+ * main: prints (using printf) the following
+ * with proper grammar, but the outcome is a piece of art,
  * Return: 0 for successful exit, non-zero for unsuccessful exit
  */
 int main(void)
