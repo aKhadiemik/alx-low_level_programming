0e2b636e87341f633e4fe2b6c97e63d02e1bbb87
Carlos Agengo <coagengo@gmail.com>
2023-02-16 02:49:04 -0800

    gcc output to specific object file, puts, printf, size of different types

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main: prints (using puts) - Programming is like building a multilungual
+ * Return: 0 if exit successful, non-zero if unsuccessful 
+ */
+
+int  main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main: prints (using printf) - with proper grammar, but the outcome is a piece of art,
+ * Return: 0 for successful exit, non-zero for unsuccessful exit 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
