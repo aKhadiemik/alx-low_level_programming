e3ec58f371739ab8ebeca32fdef0907c27cf98f4
Carlos Agengo <coagengo@gmail.com>
2023-02-21 03:23:41 -0800

    is character a member of the alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - is character a member of the alphabet
+ * @c: takes in ASCII values 
+ * Return: if member of the alphabet 1, else 0 
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
