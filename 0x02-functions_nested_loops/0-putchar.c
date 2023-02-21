2de223db67ea7e5b4b8a25507ff72cbe70dbc6a8
Carlos Agengo <coagengo@gmail.com>
2023-02-21 01:47:42 -0800

    putchar + write

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar + \n.
+ * Limitation: No std libs used 
+ * Return: Upon success 0
+ */
+
+int main(void)
+{
+	char charArray[] = "_putchar";
+	int i = 0;
+
+	while (charArray[i] != '\0')
+	{
+		_putchar(charArray[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
