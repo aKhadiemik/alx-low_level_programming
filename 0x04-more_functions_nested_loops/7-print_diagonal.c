8d147a2fc560baa1f0acccd73c7f360d0717a323
Carlos Agengo <coagengo@gmail.com>
2023-02-23 02:48:27 -0800

    Nested Loops and Functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,8 +12,9 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n == 0)
+	if (n <= 0)
 		_putchar('\n');
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
@@ -23,5 +24,4 @@ void print_diagonal(int n)
 		_putchar('\\');
 		_putchar('\n');
 	}
-
 }
