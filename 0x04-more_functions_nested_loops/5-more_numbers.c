e1a6e97a525e695cda9aa954e8901bb43170137f
Carlos Agengo <coagengo@gmail.com>
2023-02-23 17:34:52 -0800

    Loops & Fns

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
- * Description: Can only use _putchar 3 times
+ * more_numbers - prints 0-14 10 times
  */
 
 void more_numbers(void)
