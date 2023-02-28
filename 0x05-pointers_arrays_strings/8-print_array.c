45eb45de798cb759d664a884c1498346f1face86
Ngarotosto <coagengo@gmail.com>
2023-02-27 17:00:36 -0800

    Pointers + Arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < 0; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < (n -1))
-		{
+		if (i < n - 1)
 			printf(", ");
-		}
+		else
+			0;
 	}
 	printf("\n");
 }
