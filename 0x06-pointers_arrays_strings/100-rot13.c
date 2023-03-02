24d0d8700fe765656f1883f48aabd2e800c6b8f3
Ngarotosto <coagengo@gmail.com>
2023-03-02 12:44:26 -0800

    Pointers + Arrays

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,44 +2,27 @@
 
 /**
  * rot13 - encodes string using ROT13
- * @s: plaintext string
- *
+ * @s: plaintext
  * Return: encoded string
  */
-
 char *rot13(char *s)
 {
 	char *str = s;
-	char c, base;
+	char c;
 	int offset;
 
-	while (*str != '\0')
+	while (*str)
 	{
 		c = *str;
 
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		offset = (c >= 'a') ? c - 'a' : (c >= 'A' && c <= 'Z') ? c - 'A' : -1;
+		if (offset >= 0)
 		{
-			if (c >= 'a')
-			{
-				base = 'a';
-			}
-			else
-			{
-				base = 'A';
-			}
-
-			offset = c - base;
-			if (offset < 13)
-			{
-				*str = base + offset + 13;
-			}
-			else
-			{
-				*str = base + offset - 13;
-			}
+			*str = ((offset + 13) % 26) + ((c >= 'a') ? 'a' : 'A');
 		}
 
 		str++;
 	}
+
 	return (s);
 }
