75034f570571b38d23cb62331044b6bd9bd9f922
Ngarotosto <coagengo@gmail.com>
2023-03-02 05:41:37 -0800

    Pointers + Arrays

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,13 +10,14 @@ void reverse_array(int *a, int n)
 {
 	int *start, *end, tmp;
 
-	*start = a;
-	*end = a + n - 1;
 
-	if (a == NULL || n < 0)
+	if (a == (void *) 0 || n <= 0)
 	{
 		return;
 	}
+
+	start = a;
+	end = a + n - 1;
 
 	while (start < end)
 	{
