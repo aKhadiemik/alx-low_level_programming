8ce16a6c0ee5dd29a749192a108dd1d670257036
Ngarotosto <coagengo@gmail.com>
2023-03-06 15:42:27 -0800

    Memset

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,21 @@
 
 void print_chessboard(char (*a)[8])
 {
+	if (a == NULL)
+	{
+		return (-1);
+	}
+
 	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
+			if (a[i][j] < 0 || a[i][j] > 127)
+			{
+				return (-1);
+			}
 			_putchar(a[i][j]);
 			_putchar(' ');
 		}
