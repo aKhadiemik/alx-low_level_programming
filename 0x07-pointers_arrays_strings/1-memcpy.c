3659e6976d5329e38b862a48701677d588327473
Ngarotosto <coagengo@gmail.com>
2023-03-06 04:05:04 -0800

    Memset

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from src to dest
+ * @dest: memory area of destination
+ * @src: memory area of source
+ * @n: bytes to copy
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dstPtr = dest;
+	char *srcPtr = src;
+
+	while (n--)
+	{
+		*dstPtr++ = *srcPtr++;
+	}
+
+	return (dest);
+}
