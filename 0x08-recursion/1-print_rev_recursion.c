8d371dd199fb555ea68cf8e9cfef0767a26482b8
Ngarotosto <coagengo@gmail.com>
2023-03-08 02:28:20 -0800

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ *@s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (!(*s))
+	{
+		return;
+	}
+	char *next_char = s + 1;
+
+	_puts_recursion(++s);
+	_putchar(*s);
+}
