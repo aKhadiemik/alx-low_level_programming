3cf1212ae3c604826bb50ebe9fa2ea84e271fb99
Ngarotosto <coagengo@gmail.com>
2023-03-08 02:36:29 -0800

    Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,13 +7,13 @@
 
 void _print_rev_recursion(char *s)
 {
+	char *next_char;
+
 	if (!(*s))
 	{
 		return;
 	}
-	char *next_char;
 	next_char = s + 1;
-
 	_puts_recursion(next_char);
 	_putchar(*s);
 }
