f33c0de19a82f9694eb506013f88bd6487370be4
Ngarotosto <coagengo@gmail.com>
2023-03-08 04:18:11 -0800

    Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * string_length - calculates length of string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int string_length(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * is_palindrome_helper - checks is substring is palindrome
+ * @s: string
+ * @start: start of string
+ * @end: end of string
+ *
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] == s[end])
+	{
+		return (is_palindrome_helper(s, start + 1, end - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - calculates length of string and checks if palindrome
+ * @s: strign
+ *
+ *Return: 0 if not palindrome, 1 if palindrome
+ */
+
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = string_length(s);
+
+	return (is_palindrome_helper(s, 0, len - 1));
+}
