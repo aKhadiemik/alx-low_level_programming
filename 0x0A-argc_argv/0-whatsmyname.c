d9edfbd202a89a1699ab05f49e75ad7da8b8c056
Ngarotosto <coagengo@gmail.com>
2023-03-10 01:00:45 -0800

    argc & argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,9 @@
  * Return: exit with success
  */
 
-int main(int argc, char *argv[]) __attribute__((unused));
-
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
