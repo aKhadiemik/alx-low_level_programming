9ef8bb4370a60aa8a97ee82db30e429692675366
Ngarotosto <coagengo@gmail.com>
2023-03-10 00:42:09 -0800

    argc & argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of this program followed by newline
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: exit with success
+ */
+
+int main(int argc, char *argv[])
+	{
+		printf("%s\n", argv[0]);
+		return (0);
+	}
