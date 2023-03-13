15b75a8fa4d540b1de98ae7aa86ef81d19619c44
Ngarotosto <coagengo@gmail.com>
2023-03-13 03:51:54 -0700

    malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *arr; 
 
 	if (size == 0)
 	{
 		return ((void *) 0);
 	}
 
-	char *arr; 
 	arr = (char *) malloc(size * sizeof(char));
 
 	if (arr == ((void *) 0))
