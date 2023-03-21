4699974e2c4a5001c44251b3f4e0487d8435268e
Ngarotosto <coagengo@gmail.com>
2023-03-20 19:55:11 -0700

    Structures & Typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,15 +6,12 @@
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
- *
- * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == NULL)
+		d = malloc(sizeof(struct dog))
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
