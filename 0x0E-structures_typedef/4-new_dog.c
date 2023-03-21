4093c8ab60604fb274b4b8d97022b9850c95036f
Ngarotosto <coagengo@gmail.com>
2023-03-20 20:15:57 -0700

    Structures & Typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,17 +53,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
-	return (new_dog ? new_dog : NULL);
+	if (!new_dog)
+	{
+		return (NULL);
+	}
 
 	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	return ((!new_dog->name) ? (free(new_dog), NULL) : new_dog);
-	new_dog->name = _strcpy(new_dog->name, name);
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_dog->name, name);
 
 	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	return ((!new_dog->owner)
-			? (free(new_dog->name), free(new_dog), NULL)
-			: new_dog);
-	new_dog->owner = _strcpy(new_dog->owner, owner);
+	if (!new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
 
