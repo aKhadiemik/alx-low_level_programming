f454b6076a6a7dd5d97eacb1bced0754404f21e1
Ngarotosto <coagengo@gmail.com>
2023-03-22 18:43:41 -0700

    Function Pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,11 +14,11 @@
 
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
-	unsigned in i;
+	unsigned int i;
 
-	if (!array || !action)
+	if (!(array && action))
 		return;
 
 	for (i = 0; i < size; i++)
-		action(array[i]);
+		action(*(array + i));
 }
