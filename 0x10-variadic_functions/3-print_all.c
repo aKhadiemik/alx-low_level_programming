0869ddc938d23c38a0e6e1d13509a244a4a80d56
Ngarotosto <coagengo@gmail.com>
2023-03-23 10:49:34 -0700

    Variadic Fns

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,33 +3,28 @@
 #include <stdarg.h>
 
 /**
- * print_all - prints type of argument
- * @format: list of argument types
- *
- * Return: void
- */
+  *print_all - prints anything.
+  *@format: list of all arguments passed to the function.
+  *
+  *Return: void.
+  */
 void print_all(const char * const format, ...)
 {
-	const char *separator = "";
-	char *s;
-	int format_index = 0;
+	unsigned int i;
 	va_list args;
-
-	if (format == NULL)
-		return;
-
+	char *s, *separator;
 
 	va_start(args, format);
 
-	while (format[format_index])
-	{
-		if (format_index > 0)
-			separator = ", ";
+	separator = "";
 
-		switch (format[format_index])
+	i = 0;
+	while (format && format[i])
+	{
+		switch (format[i])
 		{
 			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
+				printf("%s%c", separator,  va_arg(args, int));
 				break;
 			case 'i':
 				printf("%s%d", separator, va_arg(args, int));
@@ -44,9 +39,14 @@ void print_all(const char * const format, ...)
 				printf("%s%s", separator, s);
 				break;
 			default:
-				break;
+				i++;
+				continue;
 		}
-		format_index++;
+		separator = ", ";
+		i++;
 	}
-	printf("\n"), va_end(args);
+
+	printf("\n");
+	va_end(args);
 }
+
