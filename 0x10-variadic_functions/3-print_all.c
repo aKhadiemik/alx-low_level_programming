388fc52a6aab73af473ba4ee02e658d988fab04c
Ngarotosto <coagengo@gmail.com>
2023-03-23 11:33:38 -0700

    Variadic Fns

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,7 +32,7 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				s = va_arg(args, char *);
-				if (s == NULL)
+				if (!s)
 					s = "(nil)";
 				printf("%s%s", separator, s);
 				break;
