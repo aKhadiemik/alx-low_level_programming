e18926d2acc13808aa7d09653baa7aaccee1be7d
Ngarotosto <coagengo@gmail.com>
2023-03-30 16:32:33 -0700

    Linked Lists

--- 0x12-singly_linked_lists/0-print_lists.c
+++ 0x12-singly_linked_lists/0-print_lists.c
@@ -13,12 +13,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-	if (h->str)
-		printf("[%d] %s\n", h->len, h->str);
-	else
-		printf("%d: %s\n", 0, "(nil)");
-	h = h->next;
-	elem++;
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", 0, "(nil)");
+		h = h->next;
+		elem++;
 	}
 	return (elems);
 }
