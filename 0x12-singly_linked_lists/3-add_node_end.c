8820cd9fb33710c38379c515db98a4ca0cc69431
Ngarotosto <coagengo@gmail.com>
2023-03-30 16:48:43 -0700

    Linked Lists

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds node at end of list
+ * @head: head of list
+ * @str: string
+ *
+ * Return: address to head of list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node, *node_pos;
+	size_t n;
+
+	node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	node->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	node->len = n;
+	node->next = NULL;
+	node_pos = *head;
+
+	if (node_pos == NULL)
+	{
+		*head = node;
+	}
+	else
+	{
+		while (node_pos->next != NULL)
+			node_pos = node_pos->next;
+		node_pos->next = node;
+	}
+	return (*head)
+}
