ebe91bbf2454da6153461aef566c4ff6959cb900
Ngarotosto <coagengo@gmail.com>
2023-04-04 06:34:57 -0700

    Singly Linked Lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t cnt = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		cnt++;
+		i++;
 	}
-	return (cnt);
+	return (i);
 }
