efe3156b5938b31ecb4f48b3dcd4d84ddf7e56d3
Ngarotosto <coagengo@gmail.com>
2023-04-10 18:45:17 -0700

    Bit Manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,15 +1,25 @@
 #include "main.h"
 
 /**
- * get_endianness - checks endianness
+ * flip_bits - counts bits to flip to get from int n to int m
+ * @n: integer
+ * @m: integer
  *
- * Return: if big endian 0, 1 otherwise
+ * Return: number of bits to flip
  */
-int get_endianness(void)
-{
-	int i = 1;
-	char *endianess;
 
-	endianess = (char *)&i;
-	return (*endianess);
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
+
+	while (xor_result > 0)
+	{
+		if (xor_result & 1)
+		{
+			count++;
+		}
+		xor_result >>= 1;
+	}
+	return (count);
 }
