916127e723b537ad3c2d8dbe496786d2b6add0da
Ngarotosto <coagengo@gmail.com>
2023-04-26 06:15:44 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	len = strlen(text_content);
 	write_bytes = write(fd, text_content, len);
-	if (!text_content || len && write_bytes == -1)
+	if (!text_content || (len && write_bytes == -1))
 	{
 		close(fd);
 		return (-1);
