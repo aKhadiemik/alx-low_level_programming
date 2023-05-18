2fed1b30937ac365d5213c296a57568be3b81475
Ngarotosto <carolvsreyesz@gmail.com>
2023-05-18 10:02:29 -0700

    add: fn prints all elements of a doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,21 +4,22 @@
  * print_dlistint - prints all elements of a doubly linked list
  *
  *
- * @dlistint_t: doubly linked list
+ * @h: doubly linked list
  *
  * Return: no. of elements in a doubly linked list
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-    const dlistint_t *current = h;
+	size_t count = 0;
+	const dlistint_t *current = h;
 
-    while (current != NULL) {
-        printf("%d\n", current->n);
-        count++;
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
 
-    return count;
+	return (count);
 }
