29deb5e921583e023fefb258be2b5fce1965ed77
Ngarotosto <carolvsreyesz@gmail.com>
2023-05-18 17:40:56 -0700

    add: fn frees linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	dlistint_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
