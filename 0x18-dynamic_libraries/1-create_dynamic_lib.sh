7496c7bd1e22fccf711392408ea603d93d2dd4cc
Ngarotosto <carolvsreyesz@gmail.com>
2023-05-22 18:42:46 -0700

    add: script that creates a dynamic library called liball.so from all the .c files in pwd

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
+
+for file in $c_files; do
+    gcc -c -fPIC "$file" -o "${file%.c}.o"
+done
+
+gcc -shared -o liball.so *.o
+
+rm -f *.o
