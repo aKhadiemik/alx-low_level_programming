186e76ddfb9dba32654cf19bcf485a7f8992c673
Ngarotosto <carolvsreyesz@gmail.com>
2023-06-08 11:05:12 -0700

    fix: edit to remove references to stdlib

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,33 +1,34 @@
 #include "hash_tables.h"
 
-/**
+/*
  * hash_table_create - Creates a hash table.
  * @size: size of array.
  *
  * Return: Pointer to created hash table, if error - NULL
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	/* unsigned long int i; */
 
 	if (size == 0)
-		return (NULL);
+		return ((void *)0);
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+	if (ht == (void *)0)
+		return ((void *)0);
 
 	ht->array = malloc(size * sizeof(hash_node_t *));
-	if (ht->array == NULL)
+	if (ht->array == (void *)0)
 	{
 		free(ht);
-		return (NULL);
+		return ((void *)0);
 	}
 
-	/**
+	/*
 	 *	for (i = 0; i < size; i++)
-	 *		ht->array[i] = NULL;
+	 *		ht->array[i] = (void *)0;
 	 */
 
 	ht->size = size;
