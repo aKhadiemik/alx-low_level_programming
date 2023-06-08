29505a68836caf7ac376ae68b2ce1bc445b0e660
Ngarotosto <carolvsreyesz@gmail.com>
2023-06-08 13:45:34 -0700

    add: fn prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,3 +5,51 @@
  * @ht: The hash table.
  */
 
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	_putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (flag == 1)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\'');
+			print_string(node->key);
+			_putchar('\'');
+			_putchar(':');
+			_putchar(' ');
+			_putchar('\'');
+			print_string(node->value);
+			_putchar('\'');
+			flag = 1;
+			node = node->next;
+		}
+	}
+	_putchar('}');
+	_putchar('\n');
+}
+
+/**
+ * print_string - Prints a string character by character.
+ * @str: The string to print.
+ */
+void print_string(const char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+}
